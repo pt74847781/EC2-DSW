b600b68852344ece28198af3578e4733188ffa8e
pt74847781 <134320456+pt74847781@users.noreply.github.com>
2023-07-03 19:08:54 +0000

    dceimocommit

--- src/main/java/EC2DSW/EC2DSW/Controller.java
+++ src/main/java/EC2DSW/EC2DSW/Controller.java
@@ -6,11 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController 
-@RequestMapping(path="/")
+@RestController
+@RequestMapping(path="/") //link principal padre
 public class Controller{
-    @GetMapping(path= "/")
-    public String home(){
-        return "PT74847781 - Ariel Nuñez Cruz ";
-    }
+    
+        @GetMapping(path="/") 
+        public String home(){
+            return "PT74847781 - Ariel Nuñez Cruz";
+        }
+        @GetMapping(path="/idat/codigo") 
+        public String codigo(){
+            return "PT74847781";
+        }
+        @GetMapping(path="/idat/nombre-completo") 
+        public String nombrecompleto(){
+            return "Ariel Nuñez Cruz";
+        }
 }
